edf09dbed1e3f32511e22cc02b625262a4059cce
Manik Sharma <madavsharma4545@gmail.com>
2026-03-26 20:59:34 +0530

    final database connection fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 require("dotenv").config();
-
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, 
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
