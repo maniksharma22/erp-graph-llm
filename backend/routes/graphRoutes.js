b21b0358514a508e0ef3e6bcf30d5cdffcc81d9e
Manik Sharma <114878881+maniksharma22@users.noreply.github.com>
2026-03-28 07:45:42 +0530

    Update graphRoutes.js

--- backend/routes/graphRoutes.js
+++ backend/routes/graphRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
 const { getGraph } = require("../controllers/graphController");
 
-router.get("/", getGraph);
+router.get("/graph", getGraph);
 
 module.exports = router;
